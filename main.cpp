bd86501e41896bdba32cfdea72bc256094d146e5
BaoBaB1 <lexa562@ukr.net>
2020-04-25 21:14:20 +0300

    new2

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#include <QtGui/QApplication>
-//#include <QtWidgets/QApplication>
+//#include <QtGui/QApplication>
+#include <QtWidgets/QApplication>
 #include "mainwindow.h"
+#include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
+    int b;
+    cin >> b;
     return a.exec();
 }
