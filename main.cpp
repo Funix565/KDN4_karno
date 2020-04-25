4f7d7633ef11d5e77709d93ac2069d9f5fc84e44
BaoBaB1 <lexa562@ukr.net>
2020-04-25 21:29:52 +0300

    newfunc

--- main.cpp
+++ main.cpp
@@ -22,6 +22,14 @@ int main(int argc, char *argv[])
     return a.exec();
 }
 
+bool pair (int a ){
+    if (a %2 == 0)
+        return true;
+    else {
+        return false;
+    }
+}
+
 void save (int a)
 {
     return a % 2; // new func
